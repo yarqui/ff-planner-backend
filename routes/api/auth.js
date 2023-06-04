20e58a648b7dde5a6cb53c59c1aa659a9c8bb8bd
Yaroslav Pelykh <y.pelykh@gmail.com>
2023-06-04 15:45:58 +0300

    edit register endpoint

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -4,7 +4,7 @@ const { schemas } = require("../../models/user");
 const ctrl = require("../../controllers/auth");
 const router = express.Router();
 
-router.post("/signup", validateBody(schemas.signupSchema), ctrl.signup);
+router.post("/register", validateBody(schemas.signupSchema), ctrl.signup);
 
 router.get("/verify/:verificationToken", ctrl.verifyEmail);
 
