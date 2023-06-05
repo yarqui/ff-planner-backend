4259b3591fbf9f7e0654146a4d03b4a3cb5d01bc
TaraniukD <taranyk88@ukr.net>
2023-06-05 19:02:26 +0300

    reviews completed

---
+++ routes/api/reviews.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const {isValidId, authenticate} = require("../../middlewares")
+const ctrl = require("../../controllers/reviews")
+const router = express.Router();
+
+
+// ---------------- get all ---------------------------------
+router.get("/", ctrl.getAll);
+
+// ----------------- get User Review --------------------------
+
+router.get("/auth", authenticate, ctrl.getAuthReview);
+
+//  ---------------- post review ----------------------------
+
+router.post('/', authenticate, ctrl.addReview)
+
+// //  ------------------ update -------------------------------------------
+
+router.put('/:reviewId', authenticate, isValidId, ctrl.updateReview);
+
+// //  ----------------- delete ----------------------------------------------
+
+router.delete('/:reviewId', authenticate, isValidId, ctrl.deleteReview)
+
+
+module.exports = router;
