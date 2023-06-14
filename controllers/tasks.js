8080e2fee139b685d353dcc9bd6d57c3ac63f187
Yaroslav Pelykh <y.pelykh@gmail.com>
2023-06-14 22:14:14 +0300

    change setHours

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -12,14 +12,14 @@ const getTasks = async (req, res) => {
     const targetYear = new Date(convertedDate).getFullYear();
 
     const startOfMonth = new Date(targetYear, targetMonth, 1);
-    startOfMonth.setHoursUTC(0, 0, 0, 0);
+    startOfMonth.setHours(0, 0, 0, 0);
 
     const startOfMonthKyiv = new Date(
       startOfMonth.toLocaleString("en-US", { timeZone: "Europe/Kiev" })
     );
 
     const endOfMonth = new Date(targetYear, targetMonth + 1, 0);
-    endOfMonth.setHoursUTC(23, 59, 59, 999);
+    endOfMonth.setHours(23, 59, 59, 999);
 
     const endOfMonthKyiv = new Date(
       endOfMonth.toLocaleString("en-US", { timeZone: "Europe/Kiev" })
@@ -65,13 +65,13 @@ const getTasks = async (req, res) => {
 
     // const targetDayStart = new Date(targetDate);
     const targetDayStart = new Date(targetDate);
-    targetDayStart.setHoursUTC(0, 0, 0, 0);
+    targetDayStart.setHours(0, 0, 0, 0);
     console.log("targetDayStart:", targetDayStart);
     console.log("targetDayStart:", targetDayStart.getTime());
 
     // const targetDayEnd = new Date(targetDate);
     const targetDayEnd = new Date(targetDate);
-    targetDayEnd.setHoursUTC(23, 59, 59, 999);
+    targetDayEnd.setHours(23, 59, 59, 999);
     console.log("targetDayEnd:", targetDayEnd);
     console.log("targetDayEnd:", targetDayEnd.getTime());
 
