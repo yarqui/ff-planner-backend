4b59d3a3e26c110b7041606d857fa0d068f822a3
Yaroslav Pelykh <y.pelykh@gmail.com>
2023-06-14 20:27:17 +0300

    add gravity face to cloudinary

--- cloudinary/cloudinaryOperations.js
+++ cloudinary/cloudinaryOperations.js
@@ -4,7 +4,7 @@ const { HttpError } = require("../helpers");
 const uploadAvatar = async (pathToFile) => {
   try {
     return await cloudinary.uploader.upload(pathToFile, {
-      transformation: [{ height: 600 }],
+      transformation: [{ height: 600, gravity: "face", crop: "fill" }],
     });
   } catch ({ status, message }) {
     throw HttpError(status, message);
